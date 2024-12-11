4c943281ab69f3f0054a4587ca4496d5be585017
vincentvigonte <202280295@psu.palawan.edu.ph>
2024-12-11 13:25:33 +0800

    build(api): including the database connection to animal shelter model

---
+++ api.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+from http import HTTPStatus
+
+app = Flask(__name__)
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "animal_shelter"
+
+mysql = MySQL(app)
+
+@app.route("/")
+def hello_user():
+    return "ANIMAL SHELTER API"
